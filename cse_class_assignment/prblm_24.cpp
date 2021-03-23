1cb172f90dc2a24cfd892ed3d917889b84466d4a
nusrat-jahan20 <nusrat.jahan.tasnim20@gmail.com>
2021-03-23 23:47:25 +0600

    added 5 more files

---
+++ cse_class_assignment/prblm_24.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str1[100], tmp;
+    int l, lind, rind, i;
+
+    printf("Input a string to reverse : ");
+    scanf("%s", str1);
+    l = strlen(str1);
+
+    lind = 0;
+    rind = l - 1;
+
+    for (i = lind; i < rind; i++)
+    {
+        tmp = str1[i];
+        str1[i] = str1[rind];
+        str1[rind] = tmp;
+        rind--;
+    }
+
+    printf("Reversed string is: %s\n\n", str1);
+}
